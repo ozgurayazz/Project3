0f8d6799fe6902f4c6e16ca59d378f24816b5851
User <User@OZGURPC>
2023-11-29 22:01:00 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Proje3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dil seçiniz TR - EN");
+            string dil = Console.ReadLine().ToUpper();
+
+            string msg1 = "1.sayıyı giriniz: ", msg2 = "2.sayıyı giriniz: ", msg3 = "İşlem seçiniz: ", msg4 = "İşlemin sonucu:", msg5 = "Hatalı giriş yaptınız";
+
+            if (dil == "EN")
+            {
+                msg1 = "Enter first number: ";
+                msg2 = "Enter second number";
+                msg3 = "Select operation";
+                msg4 = "Result";
+                msg5 = "İncorrect operation selected";
+            }
+
+            Console.WriteLine(msg1);
+            double sayi1 = double.Parse(Console.ReadLine());
+            Console.WriteLine(msg2);
+            double sayi2 = double.Parse(Console.ReadLine());
+            Console.WriteLine(msg3);
+            string islem = Console.ReadLine();
+            if (islem == "+")
+            {
+                double sonuc = sayi1 + sayi2;
+                Console.WriteLine(msg4 + " " + sonuc);
+            }
+            else if (islem == "-")
+            {
+                double sonuc = sayi1 - sayi2;
+                Console.WriteLine(msg4 + " " + sonuc);
+            }
+            else if (islem == "*")
+            {
+                double sonuc = sayi1 * sayi2;
+                Console.WriteLine(msg4 + " " + sonuc);
+            }
+            else if (islem == "/")
+            {
+                double sonuc = sayi1 / sayi2;
+                Console.WriteLine(msg4 + " " + sonuc);
+            }
+            else { Console.WriteLine(msg5); }
+        }
+    }
+}
